f38914a0f13f2c1d23acc18f361e25ff3fc2e172
Vitaly Fedyunin <vitalyf@fb.com>
2020-01-03 07:40:11 -0800

    Fix cudnn channels_last descriptors problem
    ghstack-source-id: 8e5049255b516c7852431c1c104209af7ecfda58
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31783

--- aten/src/ATen/native/cudnn/Conv.cpp
+++ aten/src/ATen/native/cudnn/Conv.cpp
@@ -130,13 +130,16 @@ Tensor narrowGroup(const Tensor& t, int dim, int group_idx, int64_t groups) {
   return t.narrow(dim, group_idx * group_size, group_size);
 }
 
-bool support_channels_last(const Tensor& input, const Tensor& weight, int groups) {
+bool support_channels_last(
+    const Tensor& input,
+    const Tensor& weight,
+    int groups) {
   if (!detail::getCUDAHooks().compiledWithCuDNN()) {
     return false;
   }
   long cudnn_version = detail::getCUDAHooks().versionCuDNN();
-  return (groups == 1 || cudnn_version >= 7603) &&
-      (input.suggest_memory_format() == at::MemoryFormat::ChannelsLast);
+  return (cudnn_version >= 7603) &&
+  (input.suggest_memory_format() == at::MemoryFormat::ChannelsLast);
 }
 
 // ---------------------------------------------------------------------
@@ -927,6 +930,8 @@ Tensor cudnn_convolution_forward(
 
   // See #4500
   Tensor weight_contig = weight->contiguous(layout);
+  // Make sure that NC11 strides follow formula
+  weight_contig.resize_(weight_contig.sizes(), layout);
   Tensor input_contig = input->contiguous(layout);
 
   raw_cudnn_convolution_forward_out(
@@ -1069,6 +1074,9 @@ Tensor cudnn_convolution_backward_input(
 
   // See #4500
   Tensor weight_contig = weight->contiguous(layout);
+  // Make sure that NC11 strides follow formula
+  weight_contig.resize_(weight_contig.sizes(), layout);
+
   Tensor grad_output_contig = grad_output->contiguous(layout);
 
   raw_cudnn_convolution_backward_input_out(
@@ -1109,7 +1117,6 @@ std::tuple<at::Tensor,at::Tensor,at::Tensor> cudnn_convolution_backward(
     bool benchmark, bool deterministic, std::array<bool,3> output_mask) {
 
   Tensor grad_output = grad_output_t.contiguous(input.suggest_memory_format());
-
   Tensor grad_input, grad_weight, grad_bias;
   if (input.numel() == 0) {
     if (output_mask[0]) {
@@ -1132,7 +1139,6 @@ std::tuple<at::Tensor,at::Tensor,at::Tensor> cudnn_convolution_backward(
       grad_bias = at::cudnn_convolution_backward_bias(grad_output);
     }
   }
-
   return std::tuple<Tensor,Tensor,Tensor>{grad_input, grad_weight, grad_bias};
 }
 
