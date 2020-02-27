6ad29d26b089b05b37dc706199a1e3f9528ac9e2
pritam <pritam.damania@fb.com>
2020-02-26 16:06:44 -0800

    Avoid clone for sparse tensors during accumulation of grads.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/33427
    
    This PR is an attempt to avoid clone for sparse tensors similar to how
    we avoid clone for dense tensors currently.
    
    As per my understanding even if the 'indices' and 'values' of a sparse tensor
    are non-continguous, operations like 'add' are still supported. As a result,
    the major change in this PR is to use create a shallow copy instead of clone()
    for sparse tensors.
    ghstack-source-id: 99056246
    
    Differential Revision: [D19926698](https://our.internmc.facebook.com/intern/diff/D19926698/)

--- torch/csrc/autograd/functions/accumulate_grad.h
+++ torch/csrc/autograd/functions/accumulate_grad.h
@@ -55,6 +55,24 @@ struct TORCH_API AccumulateGrad : public Node {
         // holding references to the Tensor and that is fine since these are not
         // exposed to the user.
         update_grad(new_grad_copy.detach());
+      } else if (
+          !GradMode::is_enabled() && new_grad_copy.is_sparse() &&
+          new_grad_copy._indices().is_contiguous() &&
+          new_grad_copy._values().is_contiguous() &&
+          new_grad_use_count <= num_expected_refs) {
+        // Can't detach sparse tensor (since metadata changes are not allowed
+        // after detach), so just create a new one for the grad which is a
+        // shallow copy. We need a shallow copy so that modifying the original
+        // grad tensor doesn't modify the grad we accumulate.
+        // We only skip clone if indices and values themselves are contiguous
+        // for backward compatiblity reasons. Since without this optimization,
+        // earlier we would clone the entire SparseTensor which cloned indices
+        // and values.
+        update_grad(at::sparse_coo_tensor(
+            new_grad_copy._indices(),
+            new_grad_copy._values(),
+            new_grad_copy.sizes(),
+            new_grad_copy.options()));
       } else {
         if (new_grad_copy.is_sparse()) {
           update_grad(new_grad_copy.clone());
@@ -66,9 +84,9 @@ struct TORCH_API AccumulateGrad : public Node {
       // This case is not strictly necessary, but it makes the first-order only
       // case slightly more efficient.
       if (variable_grad.is_sparse() && !new_grad_copy.is_sparse()) {
-        // If `grad_variable` is sparse and `new_grad_copy` is not sparse, their
+        // If `variable_grad` is sparse and `new_grad_copy` is not sparse, their
         // sum is not sparse, and we must change the TensorImpl type of
-        // `grad_variable` for it to store the result. However, changing the
+        // `variable_grad` for it to store the result. However, changing the
         // TensorImpl type of a tensor requires changing the tensor itself, and
         // thus in this case we have to change the grad tensor.
         update_grad(new_grad_copy + variable_grad);
@@ -76,13 +94,13 @@ struct TORCH_API AccumulateGrad : public Node {
         // In this case we can avoid changing the grad tensor. There are three
         // scenarios when we'll hit this case:
         //
-        // 1. `grad_variable` is sparse, and `new_grad_copy` is sparse.
-        // 2. `grad_variable` is dense, and `new_grad_copy` is sparse.
-        // 3. `grad_variable` is dense, and `new_grad_copy` is dense.
+        // 1. `variable_grad` is sparse, and `new_grad_copy` is sparse.
+        // 2. `variable_grad` is dense, and `new_grad_copy` is sparse.
+        // 3. `variable_grad` is dense, and `new_grad_copy` is dense.
         //
-        // In all of these three cases, `grad_variable += new_grad_copy` is a
-        // valid operation which adds `new_grad_copy` to `grad_variable` in
-        // place. `grad_variable` is thus still referring to the same tensor
+        // In all of these three cases, `variable_grad += new_grad_copy` is a
+        // valid operation which adds `new_grad_copy` to `variable_grad` in
+        // place. `variable_grad` is thus still referring to the same tensor
         // after the operation.
         variable_grad += new_grad_copy;
       }
